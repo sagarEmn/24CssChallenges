266c0fdd87a06e309d13ba03d4ba51f0fe1871de
Sagar <jussagar1@gmail.com>
2024-08-24 22:49:09 +0545

    half done

--- 2-Search Bar/src/App.tsx
+++ 2-Search Bar/src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <>
-  
-  Eminem</>;
+  return (
+    <>
+      <div className="container">
+        <label htmlFor="search-field" className="hide">Search</label>
+        <input
+          type="search"
+          className="search-bar"
+          id="search-field"
+          placeholder="Search..."
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
