38fbe65cf170f7968018cde8ec466de45d2867e7
Sagar <jussagar1@gmail.com>
2024-08-27 21:58:02 +0545

    HTML file finished

--- 3-Codepen Tile/src/App.tsx
+++ 3-Codepen Tile/src/App.tsx
@@ -1,12 +1,31 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <div className="card">
+        <div className="card-thumbnail">
+          <img
+            alt="eminem award picture"
+            src="/eminem-picture.jpg"
+          />
+        </div>
+        <div className="card-body">
+          <div className="card-avatar">
+            <img alt="" src="/eminem-picture.jpg" />
+          </div>
+          <div className="card-details">
+            <h3>Eminem | Slim Shady</h3>
+            <p>RAP GOD</p>
+          </div>
+        </div>
+        <ul className="card-stats">
+          <li>💖 51 Years Old</li>
+          <li>💖 8 Mile Nordic Track</li>
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
