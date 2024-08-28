37c7982208c5c8cf274cff71fd770b613fffe26b
Sagar <jussagar1@gmail.com>
2024-08-28 11:13:38 +0545

    finished #1

--- 3-Codepen Tile/src/App.tsx
+++ 3-Codepen Tile/src/App.tsx
@@ -5,27 +5,29 @@ function App() {
     <>
       <div className="card">
         <div className="card-thumbnail">
-          <img
-            alt="eminem award picture"
-            src="/eminem-picture.jpg"
-          />
+          <img alt="eminem award picture" src="/eminem-picture.jpg" />
         </div>
         <div className="card-body">
-          <div className="card-avatar">
-            <img alt="" src="/eminem-picture.jpg" />
+          <div className="card-firstrow">
+            <div className="card-avatar">
+              <img alt="" src="/eminem-picture.jpg" />
+            </div>
+            <div className="card-details">
+              <h3>Eminem | Slim Shady</h3>
+              <p>RAP GOD</p>
+            </div>
           </div>
-          <div className="card-details">
-            <h3>Eminem | Slim Shady</h3>
-            <p>RAP GOD</p>
+          <div className="card-stats">
+            <ul>
+              <li>💖 51 Years Old</li>
+              <li>💖 8 Mile Nordic Track</li>
+            </ul>
           </div>
         </div>
-        <ul className="card-stats">
-          <li>💖 51 Years Old</li>
-          <li>💖 8 Mile Nordic Track</li>
-        </ul>
       </div>
     </>
   );
 }
 
 export default App;
+``;
