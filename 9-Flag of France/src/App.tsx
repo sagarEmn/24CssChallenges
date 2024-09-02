96463c0f8e43570c836edc6b558dc044f42e0765
Sagar <jussagar1@gmail.com>
2024-09-02 10:13:44 +0545

    Project 9 Finished

---
+++ 9-Flag of France/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flag-france">
+        <div className="blue stripe"></div>
+        <div className="white stripe"></div>
+        <div className="red stripe"></div>
+      </div>
+    </>
+  )
+}
+
+export default App
