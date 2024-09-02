f0f01490764606b4db33839e1ec1fd9db994c3ac
Sagar <jussagar1@gmail.com>
2024-09-02 18:46:57 +0545

    #10 Flag of Germany - half-done

---
+++ 10-Flag of Germany/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flag-germany">
+        <div className="black stripe"></div>
+        <div className="red stripe"></div>
+        <div className="yellow stripe"></div>
+      </div>
+    </>
+  )
+}
+
+export default App
