5b1d823194b1c41bae57234b9c8f31067e367c6f
Sagar <jussagar1@gmail.com>
2024-09-03 15:45:00 +0545

    Flag of madagascar layout completed using flex

---
+++ 11-Flag of Madagascar/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flag-madagascar">
+        <div className="white stripe"></div>
+        <div className="red-green">
+          <div className="red stripe"></div>
+          <div className="green stripe"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
