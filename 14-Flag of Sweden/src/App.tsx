8f9194bdb23bae8c8fa7f6dda21362af4be68588
Sagar <jussagar1@gmail.com>
2024-09-03 21:43:28 +0545

    #14 Flag of Sweden Finished

--- 14-Flag of Sweden/src/App.tsx
+++ 14-Flag of Sweden/src/App.tsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
-      
+      <div className="flag-sweden">
+        <div className="horizontal-stripe"></div>
+        <div className="vertical-stripe"></div>
+      </div>
     </>
   )
 }
