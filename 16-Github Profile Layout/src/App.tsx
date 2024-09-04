444610f212416160c205d74663a1f89136b263c2
Sagar <jussagar1@gmail.com>
2024-09-04 12:24:44 +0545

    Initiated project, cleared out stock code

---
+++ 16-Github Profile Layout/src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="profile-container">
+        <div className="profile-content">
+          <img alt="" src="16-Github Profile Layout\public\code.jpg" />
+          <div className="profile-details">
+            <h2>Coder Name</h2>
+            <h3>scrimsallday3000</h3>
+            <p>I write code and eat foods. </p>
+            <button type="button">Edit Profile</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
