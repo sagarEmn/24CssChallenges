05fe30bda7141bf7c818c106b2faabe98fd6d430
Sagar <jussagar1@gmail.com>
2024-09-04 17:54:29 +0545

    Added Switch

--- 17-Toggle Switch/src/App.tsx
+++ 17-Toggle Switch/src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-      
+      <div className="toggle-wrap">
+        <label htmlFor="toggle">
+          <input type="checkbox" id="toggle" className="toggle-input"></input>
+          <div className="toggle-switch"></div>
+        </label>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
