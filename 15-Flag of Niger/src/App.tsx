45a063f0b1961abd839f987d841a47f9a98d0c4f
Sagar <jussagar1@gmail.com>
2024-09-04 11:29:37 +0545

    #15 Flag of Niger initiated, redundant code cleared

---
+++ 15-Flag of Niger/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="flag-niger">
+        <div className="red stripe"></div>
+        <div className="white stripe">
+          <div className="red-circle"></div>
+        </div>
+        <div className="green stripe"></div>
+      </div>
+    </>
+  )
+}
+
+export default App
