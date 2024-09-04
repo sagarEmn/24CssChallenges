f3e3d60994eab3b5a7d6198fd0442923ffbc480f
Sagar <jussagar1@gmail.com>
2024-09-04 20:58:04 +0545

    #18 Playing Card - Ace of Spades Finished

---
+++ 18-Playing Card - Ace of Spades/src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="card">
+          <div className="top">
+            <div>A</div>
+            <div>&spades;</div>
+          </div>
+          <div className="center">&spades;</div>
+          <div className="bottom">
+            <div>A</div>
+            <div>&spades;</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
