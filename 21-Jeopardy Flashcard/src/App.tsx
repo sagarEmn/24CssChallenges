f68e735e8162acb558031d38e41d4cbf4715bbd5
Sagar <jussagar1@gmail.com>
2024-09-05 13:04:07 +0545

    Added contents

---
+++ 21-Jeopardy Flashcard/src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <div className="card-front">
+          <h2>
+            This is now deprecated HTML tag, popular in the early days of the
+            internet, magically made text scroll across the screen
+          </h2>
+        </div>
+        <div className="card-back">
+          <h2>What is the marquee tag</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
