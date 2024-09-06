921fd9226930ba91c35914d92943d36776ff3c80
Sagar <jussagar1@gmail.com>
2024-09-06 22:27:06 +0545

    Image replaced

--- 23-Instagram Stories Menu/src/App.tsx
+++ 23-Instagram Stories Menu/src/App.tsx
@@ -6,8 +6,21 @@ function App() {
       <ul className="stories-menu">
         <li className="story new">
           <div className="img-wrapper">
-            <img className="img" src="scott-webb-unsplash.jpg" alt="" />
+            <img className="img" src="momo1.jpeg" alt="" />
           </div>
+          <p>Julian</p>
+        </li>
+        <li className="story new">
+          <div className="img-wrapper">
+            <img className="img" src="momo2.jpg" alt="" />
+          </div>
+          <p>Julian</p>
+        </li>
+        <li className="story new">
+          <div className="img-wrapper">
+            <img className="img" src="momo3.jpg" alt="" />
+          </div>
+          <p>Julian</p>
         </li>
       </ul>
     </>
