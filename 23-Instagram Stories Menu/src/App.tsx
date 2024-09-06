07fa082488873cb7ed87e15799568135b2c84ca7
Sagar <jussagar1@gmail.com>
2024-09-06 22:09:49 +0545

    Added Images in public

--- 23-Instagram Stories Menu/src/App.tsx
+++ 23-Instagram Stories Menu/src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-      
+      <ul className="stories-menu">
+        <li className="story new">
+          <div className="img-wrapper">
+            <img className="img" src="scott-webb-unsplash.jpg" alt="" />
+          </div>
+        </li>
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
